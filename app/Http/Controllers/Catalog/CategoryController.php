211fe8d0195e0a4273f51aae9846dab71235c9e2
Iffi <irfan.elahi187@gmail.com>
2021-10-30 20:46:06 +0500

    new things

--- app/Http/Controllers/Catalog/CategoryController.php
+++ app/Http/Controllers/Catalog/CategoryController.php
@@ -107,9 +107,8 @@ class CategoryController extends Controller
     public function destroy($id)
     {
         $category = Category::find($id);
-        // dd();
         foreach($category->products as $p){
-            $ca = Cart::whereIn('product_id',$p->id)->get();
+            $ca = Cart::where('product_id',$p->id)->get();
             $oa = Order::all();
             foreach($ca as $c){
                 $c->delete();
